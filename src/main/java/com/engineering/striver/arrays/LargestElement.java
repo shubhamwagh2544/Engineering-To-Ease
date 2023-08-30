5fca70d7c3ed0fb5646c9cfd35d5dbda47cea071
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-08-31 01:33:39 +0530

    largest element

--- src/main/java/com/engineering/striver/arrays/LargestElement.java
+++ src/main/java/com/engineering/striver/arrays/LargestElement.java
@@ -1,12 +1,26 @@
 package com.engineering.striver.arrays;
 
+import java.util.Arrays;
+
 public class LargestElement {
     public static void main(String[] args) {
         int[] array = {2, 5, 1, 3, 0, 5, 1, 3, 5};
         int n = array.length;
 
+        //brute
+        //TC : O(NLogN)
+        //SC : O(N)
+        largestElement(array, n);
+
+        //optimal
+        //TC : O(N)
         largestElementInArray1(array, n);
         largestElementInArray2(array);
+    }
+
+    private static void largestElement(int[] array, int n) {
+        Arrays.sort(array);
+        System.out.println(array[n-1]);
     }
 
     private static void largestElementInArray1(int[] array, int n) {
