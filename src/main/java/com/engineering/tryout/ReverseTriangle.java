8af8ef6dd89b2f8dce9d280adcd054718764cca1
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-09-04 17:53:06 +0530

    pattern

---
+++ src/main/java/com/engineering/tryout/ReverseTriangle.java
@@ -0,0 +1,20 @@
+package com.engineering.tryout;
+
+import com.engineering.striver.recursion.ScannerProvider;
+
+public class ReverseTriangle {
+    public static void main(String[] args) {
+        int n = ScannerProvider.scanner().nextInt();
+
+        printReverse(n);
+    }
+
+    private static void printReverse(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
