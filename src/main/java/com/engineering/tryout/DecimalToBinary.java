c796ac845877b29e9cb83dff28e90c48f27e767b
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-09-06 01:27:55 +0530

    new problems

---
+++ src/main/java/com/engineering/tryout/DecimalToBinary.java
@@ -0,0 +1,39 @@
+package com.engineering.tryout;
+
+import com.engineering.striver.recursion.ScannerProvider;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        int n = ScannerProvider.scanner().nextInt();
+
+        //simple
+        decimalToBinary(n);
+
+        //bit manipulation
+        decimalToBinaryAnother(n);
+    }
+
+    private static void decimalToBinaryAnother(int n) {
+        StringBuilder binary = new StringBuilder();
+
+        while (n != 0) {
+            int bit = n & 1;
+            binary.append(bit);
+            n = n >> 1;
+        }
+
+        System.out.println(binary.reverse());
+    }
+
+    private static void decimalToBinary(int n) {
+        StringBuilder binary = new StringBuilder();
+
+        while (n != 0) {
+            int remainder = n % 2;
+            binary.append(remainder);
+            n = n / 2;
+        }
+
+        System.out.println(binary.reverse());
+    }
+}
