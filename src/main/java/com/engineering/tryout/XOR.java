5877fcbf8284b988acbf5deff022f88ead766fb6
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-09-06 20:44:21 +0530

    Single Number

---
+++ src/main/java/com/engineering/tryout/XOR.java
@@ -0,0 +1,10 @@
+package com.engineering.tryout;
+
+public class XOR {
+    public static void main(String[] args) {
+        //1 : XOR of two same numbers is always zero
+        System.out.println(1^1);        //0
+        System.out.println(1^1^1);      //0^1 = 1
+        System.out.println(1^1^1^1);    //0^0 = 0
+    }
+}
