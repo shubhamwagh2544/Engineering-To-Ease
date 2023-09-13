dbd100176de3b1cf0025a066bfcb541aa0ffc9e7
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-09-13 13:00:49 +0530

    Print Primes Till N

--- src/main/java/com/engineering/tryout/PrintPrimesTillN.java
+++ src/main/java/com/engineering/tryout/PrintPrimesTillN.java
@@ -1,2 +1,46 @@
-package com.engineering.tryout;public class PrintPrimesTillN {
+package com.engineering.tryout;
+
+import com.engineering.striver.recursion.ScannerProvider;
+
+public class PrintPrimesTillN {
+    public static void main(String[] args) {
+        int n = ScannerProvider.scanner().nextInt();
+
+        //TC : O(n ^ 2)
+        //SC : O(1)
+        brute(n);
+
+        //TC : O(n ^ 3/2)
+        better(n);
+
+        //TC : O(n * log(logn))
+        //SC : O(1)
+        optimal(n);
+    }
+
+    private static void optimal(int n) {
+
+
+    }
+
+    private static void better(int n) {
+
+    }
+
+    private static void brute(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (ifPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    private static boolean ifPrime(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
