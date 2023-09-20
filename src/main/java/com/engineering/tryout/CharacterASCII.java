faba2913e8d3d34faffbec857a6917cb513f19ce
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-09-20 20:57:50 +0530

    typecasting

--- src/main/java/com/engineering/tryout/CharacterASCII.java
+++ src/main/java/com/engineering/tryout/CharacterASCII.java
@@ -4,5 +4,9 @@ public class CharacterASCII {
     public static void main(String[] args) {
         char letter = 'a';
         System.out.println((int) letter);
+
+        //typecasting
+        int x = 'a';
+        System.out.println(x);
     }
 }
