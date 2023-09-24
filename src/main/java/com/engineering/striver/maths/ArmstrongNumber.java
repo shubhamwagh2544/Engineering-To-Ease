781305154e1428e5488fe2dfce372af8d723f821
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-09-24 21:38:20 +0530

    basic maths

---
+++ src/main/java/com/engineering/striver/maths/ArmstrongNumber.java
@@ -0,0 +1,25 @@
+package com.engineering.striver.maths;
+
+import com.engineering.striver.recursion.ScannerProvider;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        int n = ScannerProvider.scanner().nextInt();
+
+        isArmStrong(n);
+    }
+
+    private static void isArmStrong(int n) {
+        int cubeSum = 0;
+        int dummy = n;
+
+        while (n != 0) {
+            int rem = n % 10;
+            cubeSum += rem * rem * rem;
+            n = n / 10;
+        }
+
+        if (cubeSum == dummy) System.out.println("ArmStrong");
+        else System.out.println("Not");
+    }
+}
