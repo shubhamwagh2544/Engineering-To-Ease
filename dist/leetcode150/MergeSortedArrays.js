638db4e888ca402d9887f4cee724aec49d483757
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-02-07 01:47:21 +0530

    typescript into action

---
+++ dist/leetcode150/MergeSortedArrays.js
@@ -0,0 +1,6 @@
+"use strict";
+function mergeSortedArrays(num1, num2) {
+    console.log('hey there');
+    return [1, 2, 3];
+}
+console.log(mergeSortedArrays([1, 1], [1, 2]));
--- src/leetcode150/MergeSortedArrays.ts
+++ src/leetcode150/MergeSortedArrays.ts
@@ -1,5 +1,3 @@
-// TODO: write in typescript
-
 function mergeSortedArrays(num1: number[], num2: number[]): number[] {
     console.log('hey there')
     return [1, 2, 3]
