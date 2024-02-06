70b78d35a0941099ea107288b745314ec72aae40
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-02-07 01:34:51 +0530

    leetcode 150 challenge

---
+++ src/leetcode150/MergeSortedArrays.ts
@@ -0,0 +1,8 @@
+// TODO: write in typescript
+
+function mergeSortedArrays(num1: number[], num2: number[]): number[] {
+    console.log('hey there')
+    return [1, 2, 3]
+}
+
+console.log(mergeSortedArrays([1, 1], [1, 2]))
