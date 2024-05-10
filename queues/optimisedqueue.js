35d2b8de8a041f8f4fbab08fc426f76e03fa8259
shubham.wagh <shubham.wagh@globant.com>
2024-05-10 08:42:06 +0530

    optimised queue

---
+++ queues/optimisedqueue.js
@@ -0,0 +1,57 @@
+class Queue {
+
+    constructor() {
+        this.data = {}
+        this.front = 0
+        this.rear = 0
+    }
+
+    // enqueue
+    enqueue(item) {
+        this.data[this.rear] = item
+        this.rear++
+    }
+
+    // dequeue
+    dequeue() {
+        const item = this.data[this.front]
+        delete this.data[this.front]
+        this.front++
+        return item
+    }
+
+    // isEmpty
+    isEmpty() {
+        return this.rear - this.front === 0
+    }
+
+    // peek
+    peek() {
+        return this.data[this.front]
+    }
+
+    // size
+    size() {
+        return this.rear - this.front
+    }
+
+    // print
+    printQueue() {
+        console.log(this.data)
+    }
+}
+
+const queue = new Queue()
+console.log(queue)
+console.log(queue.isEmpty())
+
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+
+queue.printQueue()
+
+console.log(queue.dequeue())
+console.log(queue.peek())
+
+console.log(queue.size())
