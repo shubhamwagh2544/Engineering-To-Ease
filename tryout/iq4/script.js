73a3d218d882c517ffdcad59237a7aba8368ec63
shubham.wagh <shubham.wagh@globant.com>
2024-05-14 09:58:25 +0530

    async await wo promise

--- tryout/iq4/script.js
+++ tryout/iq4/script.js
@@ -1,11 +1,36 @@
 // resolves promise when button is clicked
 
 const promise = new Promise((res, rej) => {
-    document.getElementById('btn').addEventListener('click', () => {
+    document.getElementById('btn-res').addEventListener('click', () => {
         res('resolved')
+    })
+    document.getElementById('btn-rej').addEventListener('click', () => {
+        rej('rejected')
     })
 })
 console.log(promise)
 
-promise.then((val) => console.log(val))
-console.log(promise)
+promise
+    .then((val) => console.log(val))
+    .catch((val) => console.log(val))
+
+//promise
+//    .catch((val) => console.log(val))
+
+
+// create promise without promise syntax :=> async
+async function myFunction() {
+    // resolve promise
+    const res = await promise;
+    console.log(res)
+    return "new resolved"
+
+    // rejected
+    //throw new Error
+
+    // resolved
+    //return 'hello world'
+}
+const newPromise = myFunction()
+const newRes = await newPromise
+console.log(newRes)
