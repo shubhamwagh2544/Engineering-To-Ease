cb26c6c9c7c2d912d30b1d2a9c4bc007d3b92d62
shubham.wagh <shubham.wagh@globant.com>
2024-05-14 09:13:25 +0530

    typeof function class array

---
+++ tryout/iq3.js
@@ -0,0 +1,17 @@
+// 1
+function call() {
+
+}
+console.log(typeof call)                // function
+
+// 2
+class A {
+    constructor() {
+
+    }
+}
+console.log(typeof A)                   // function
+
+// 3
+const array = [1, 2, 3]
+console.log(typeof array)               // object
