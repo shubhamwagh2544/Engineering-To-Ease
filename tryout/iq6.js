a597ce9a2de8a547f97d517f4ff65212bc23f01f
shubham.wagh <shubham.wagh@globant.com>
2024-05-29 20:57:33 +0530

    iv q6 js

---
+++ tryout/iq6.js
@@ -0,0 +1,8 @@
+const obj = {
+    prop1: 'val1',
+    prop2: 'prop2',
+    prop3: 'prop3'
+}
+
+console.log(obj.prop1)          // val1
+console.log(obj.prop5)          // no issue in javascript (undefined)
