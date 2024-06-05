e67f08fa9063506d841646ff9c3792f23fc21667
shubham.wagh <shubham.wagh@globant.com>
2024-06-05 22:36:13 +0530

    test files

---
+++ js-iv/src/temp.js
@@ -0,0 +1,9 @@
+// console.log(String.fromCharCode(0 + 'a'.charCodeAt(0)))
+
+const func = 'www'
+//() => { }                     // function
+//{}                            // object
+//['ss']                        // object
+//function () { }               // function
+//console.log(typeof func)
+
